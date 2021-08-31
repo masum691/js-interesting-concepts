bb2e44b71766bac50b7915ba735d908ff34830ce
masum691 <masumhu691@gmail.com>
2021-08-31 12:22:08 +0600

    double and triple equal

---
+++ double-triple-equal.js
@@ -0,0 +1,46 @@
+
+
+/*
+
+== check kore 2ta man(value) soman ki na
+(double equal 2 ta value k convert kore check kore)
+double equal convert korar somoy true k 1 bani, false k 0 banai
+== k avoid korai valo compare er somoy
+
+=== check kore type and value soman ki na
+most of the case soman ki na compare korle === use korte hobe
+
+explesive conversion - nije kore dewa
+
+*/
+
+// const first = 2;
+// const second = true;
+
+// if(first == second){
+//     console.log('true')
+// }
+// else{
+//     console.log('false')
+// }
+
+const first = '0';
+const second = false;
+
+if(first == second){
+    console.log('true')
+}
+else{
+    console.log('false')
+}
+
+
+const a = {name: 'john'};
+const b = {name: 'john'};
+if(a==b){
+    console.log('same');
+}
+else{
+    console.log('not name');
+}
+
