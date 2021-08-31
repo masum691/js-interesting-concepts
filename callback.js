76f21d05e415ac85155b181e3c5e5c227187ac81
masum691 <masumhu691@gmail.com>
2021-08-31 15:49:43 +0600

    callback

---
+++ callback.js
@@ -0,0 +1,35 @@
+
+// function bestFriend(name){
+//     console.log(name)
+// }
+// const name = 'Samir joy';
+// bestFriend(name);
+
+function bestFriends(name){
+    console.log(name)
+    
+}
+const names = ['samir joy', 'sabbir', 'maruf'];
+bestFriends(names);
+
+
+function friend(about){
+    console.log(about)
+}
+const aboutHim = {
+    name: 'Forid',
+    age: 21,
+    height: 5.7,
+    country: 'Bangladesh'
+}
+friend(aboutHim);
+
+
+
+function weatherInACountry(weather){
+    
+}
+function sunny(){
+    console.log('sunny weather');
+}
+weatherInACountry(sunny())
